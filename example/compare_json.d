1e28c62d47a99ad99fee83de2b3e98805db07878
Masahiro Nakagawa <repeatedly@gmail.com>
2010-04-21 22:10:14 +0900

    Fix example/compare_json.d

--- example/compare_json.d
+++ example/compare_json.d
@@ -13,24 +13,21 @@ import msgpack.msgpack;
 
 void main()
 {
-    JSONValue jsonObj;
-    jsonObj.array = [parseJSON(`[12, "foo", true, 0.23]`)];
-    jsonObj.type  = JSON_TYPE.ARRAY;
+    JSONValue jsonObj = parseJSON(`[12, "foo", true, 0.23]`);
 
     void f1()
     {
         parseJSON(toJSON(&jsonObj));
     }
 
-    mp_Object mpObj;
-    mpObj = mp_Object([mp_Object(12UL), mp_Object(cast(ubyte[])"foo"), mp_Object(true), mp_Object(0.23)]);
+    mp_Object mpObj = mp_Object([mp_Object(12UL), mp_Object(cast(ubyte[])"foo"), mp_Object(true), mp_Object(0.23)]);
 
     void f2()
     {
         unpack(pack(mpObj));
     }
 
-    auto results = benchmark!(f1, f2)(10000);
-    writeln("JSON:    ", results[0]);
-    writeln("Msgpack: ", results[1]);
+    auto times = benchmark!(f1, f2)(10000);
+    writeln("JSON:    ", times[0]);
+    writeln("Msgpack: ", times[1]);
 }
